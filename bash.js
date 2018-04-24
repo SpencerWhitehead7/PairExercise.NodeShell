a6141706ab45388e8b8c1a55397a062bd6afcc23
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-04-24 13:30:39 -0400

    refactors (again)

--- bash.js
+++ bash.js
@@ -6,15 +6,16 @@ process.stdout.write('\nprompt > ')
 process.stdin.on('data', (data) => {
   const input = data.toString().trim().split(" ")
   const command = input[0]
-  const args = input.slice[1]
+  const args = input.slice(1)
   if(command === "pwd"){
     pwd()
   }else if(command === "ls"){
     ls()
   }else if(command === "cat"){
-    cat(args)
+    cat(args[0])
   }else{
     process.stdout.write("Sorry, we don't support that one.")
   }
-  process.stdout.write('\nprompt > ')
+  setTimeout(function(){
+    process.stdout.write('\nprompt > ')}, 0)
 })
--- ls.js
+++ ls.js
@@ -3,9 +3,9 @@ const fs = require("fs")
 module.exports = function() {
   fs.readdir("./", "utf8", (err, files)=>{
     if(err){
-          throw err
-        }else{
-          process.stdout.write(files.join("\n"))
-        }
-      })
+      throw err
+    }else{
+      process.stdout.write(files.join("\n"))
+    }
+  })
 }
